c8d2b92a40bd3f549a49d2b4c1447a2600539d45
Juan Mendez <contact@juanmendez.info>
2016-11-15 20:38:06 -0600

    this is the base of the whole application

---
+++ app/src/main/java/info/juanmendez/md/befamiliar/MainActivity.java
@@ -0,0 +1,9 @@
+package info.juanmendez.md.befamiliar;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+}
