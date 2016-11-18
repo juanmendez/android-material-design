cdd57a92222326e1a2b44b5022b38d23e13c350f
Juan Mendez <contact@juanmendez.info>
2016-11-18 17:13:36 -0600

    added toolbar and customized some attributes

--- app/src/main/java/info/juanmendez/md/befamiliar/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/befamiliar/MainActivity.java
@@ -2,19 +2,29 @@ package info.juanmendez.md.befamiliar;
 
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
+    @ViewById(R.id.toolbar)
+    Toolbar toolbar;
+
     @AfterViews
     void afterViews(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ){
 
+        setSupportActionBar( toolbar );
+        getSupportActionBar().setIcon( R.drawable.good_day);
+        getSupportActionBar().setTitle("Welcome!");
+        getSupportActionBar().setSubtitle("Folks!");
+        toolbar.setNavigationIcon( R.drawable.navigation_back);
+
+        /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ){
         }else{
-
-        }
+        }*/
     }
 }
