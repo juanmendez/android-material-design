095af92e0138d489a5ed050af0c22303ac99f47b
Juan Mendez <contact@juanmendez.info>
2016-11-18 17:48:52 -0600

    created menu items and set up a theme

--- app/src/main/java/info/juanmendez/md/befamiliar/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/befamiliar/MainActivity.java
@@ -3,12 +3,16 @@ package info.juanmendez.md.befamiliar;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.main_menu)
 public class MainActivity extends AppCompatActivity {
 
     @ViewById(R.id.toolbar)
@@ -26,5 +30,34 @@ public class MainActivity extends AppCompatActivity {
         /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ){
         }else{
         }*/
+    }
+
+    @OptionsItem
+    void discard(){
+        toastForMe( "discard" );
+    }
+
+    @OptionsItem
+    void search(){
+        toastForMe( "search" );
+    }
+
+    @OptionsItem
+    void settings(){
+        toastForMe( "settings" );
+    }
+
+    @OptionsItem
+    void edit(){
+        toastForMe( "edit" );
+    }
+
+    @OptionsItem
+    void exit(){
+        toastForMe( "exit" );
+    }
+
+    private void toastForMe( String msg ){
+        Toast.makeText( this, msg, Toast.LENGTH_LONG ).show();
     }
 }
