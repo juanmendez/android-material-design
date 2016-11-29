e2255c80c74401b947513166b3587d96ad573c23
Juan Mendez <contact@juanmendez.info>
2016-11-28 18:51:16 -0600

    adding navigation drawer

---
+++ app/src/main/java/info/juanmendez/md/befamiliar/app/NavigationDrawerFragment.java
@@ -0,0 +1,17 @@
+package info.juanmendez.md.befamiliar.app;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import info.juanmendez.md.befamiliar.R;
+
+
+/**
+ * Created by Juan Mendez on 11/28/2016.
+ * www.juanmendez.info
+ * contact@juanmendez.info
+ */
+@EFragment(R.layout.fragment_navigation_drawer)
+public class NavigationDrawerFragment extends Fragment {
+}
